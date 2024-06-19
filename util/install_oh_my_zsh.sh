76cff6a563cbbe72a9393a6dcbc6caebe00f5db0
Bart Dabek <bart@flowpress.com>
2024-06-19 12:21:35 -0400

    Installation work... wip

--- util/install_oh_my_zsh.sh
+++ util/install_oh_my_zsh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Source the .fpd-shellrc file to get logging functions
-ls -la ~
-source ~/.fpd-shell/.fpd-shellrc
 
 install_oh_my_zsh() {
+	ls -la ~/.fpd-shell/
+	source ~/.fpd-shell/.fpd-shellrc
 	log_heading "Installing Oh My Zsh..."
 	RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
