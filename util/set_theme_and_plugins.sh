ebdaa7b3a014654142e1a31d6b1262bf51cf9d9a
Bart Dabek <bart@flowpress.com>
2024-06-19 13:15:24 -0400

    Installation work... wip

--- util/set_theme_and_plugins.sh
+++ util/set_theme_and_plugins.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
 set_oh_my_zsh_theme_and_plugins() {
-  # Create .zshrc if it doesn't exist
-  if [ ! -f ~/.zshrc ]; then
-    touch ~/.zshrc
-  fi
+	# Create .zshrc if it doesn't exist
+	if [ ! -f ~/.zshrc ]; then
+		touch ~/.zshrc
+	fi
 
-  echo "Select a theme for Oh My Zsh or type your custom theme:"
-  echo "1) robbyrussell"
-  echo "2) agnoster"
-  echo "3) gallois"
-  echo "4) avit"
-  echo "5) random"
-  echo "6) Custom theme"
-  echo -n "Enter the number corresponding to your choice or type your theme name: "
-  read theme_choice
+	echo "Select a theme for Oh My Zsh or type your custom theme:"
+	echo "1) robbyrussell"
+	echo "2) agnoster"
+	echo "3) gallois"
+	echo "4) avit"
+	echo "5) random"
+	echo "6) Custom theme"
+	echo -n "Enter the number corresponding to your choice or type your theme name: "
+	read theme_choice
 
-  case $theme_choice in
-    1)
-      theme="robbyrussell"
-      ;;
-    2)
-      theme="agnoster"
-      ;;
-    3)
-      theme="gallois"
-      ;;
-    4)
-      theme="avit"
-      ;;
-    5)
-      theme="random"
-      ;;
-    6)
-      echo -n "Enter your custom theme name: "
-      read custom_theme
-      theme="$custom_theme"
-      ;;
-    *)
-      echo "Invalid choice, defaulting to robbyrussell"
-      theme="robbyrussell"
-      ;;
-  esac
+	case $theme_choice in
+	1)
+		theme="robbyrussell"
+		;;
+	2)
+		theme="agnoster"
+		;;
+	3)
+		theme="gallois"
+		;;
+	4)
+		theme="avit"
+		;;
+	5)
+		theme="random"
+		;;
+	6)
+		echo -n "Enter your custom theme name: "
+		read custom_theme
+		theme="$custom_theme"
+		;;
+	*)
+		echo "Invalid choice, defaulting to robbyrussell"
+		theme="robbyrussell"
+		;;
+	esac
 
-  # Set the theme in .zshrc
-  sed -i.bak "s/^ZSH_THEME=.*/ZSH_THEME=\"$theme\"/" ~/.zshrc
+	# Set the theme in .zshrc
+	sed -i.bak "s/^ZSH_THEME=.*/ZSH_THEME=\"$theme\"/" ~/.zshrc
 
-  # Add plugins to .zshrc before sourcing oh-my-zsh
-  sed -i.bak '/^source $ZSH\/oh-my-zsh.sh/i plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
+	# Add plugins to .zshrc before sourcing oh-my-zsh
+	sed -i.bak '/^source $ZSH\/oh-my-zsh.sh/i plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' ~/.zshrc
 
-  echo "Theme set to $theme and plugins configured."
+	echo "Theme set to $theme and plugins configured."
 }
