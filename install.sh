2184f09f1ec0e83617d4c6d650249038357d33ce
Bart Dabek <bart@flowpress.com>
2024-06-19 09:56:43 -0400

    Installation work... wip

--- install.sh
+++ install.sh
@@ -13,46 +13,46 @@ source "$SCRIPT_DIR/util/print_success_message.sh"
 read -p "Do you want to install or uninstall FPD Shell? (i/u): " action
 
 case $action in
-  i|install)
-    read -p "Do you want to install Oh My Zsh? (y/n): " install_omz
-    if [ "$install_omz" == "y" ]; then
-      if [ -d ~/.oh-my-zsh ]; then
-        echo "The \$ZSH folder already exists ($HOME/.oh-my-zsh)."
-        echo "You'll need to remove it if you want to reinstall."
-      else
-        install_oh_my_zsh
-        set_oh_my_zsh_theme_and_plugins
-        echo 'print_success_message' >> ~/.zshrc
-      fi
-    else
-      print_success_message
-    fi
-
-    # Clone the repository
-    if [ -d ~/.fpd-shell ]; then
-      echo "~/.fpd-shell already exists. Please remove it or choose another directory."
-    else
-      echo "Cloning the FPD Shell repository..."
-      git clone https://github.com/FlowPress/fpd-shell.git ~/.fpd-shell
-
-      # Source the main shell script in .bashrc or .zshrc
-      if [ -f ~/.zshrc ]; then
-        echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.zshrc
-        source ~/.zshrc
-      elif [ -f ~/.bashrc ]; then
-        echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.bashrc
-        source ~/.bashrc
-      else
-        # Default to creating .bashrc if neither exists
-        echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.bashrc
-        source ~/.bashrc
-      fi
-    fi
+    i|install)
+        read -p "Do you want to install Oh My Zsh? (y/n): " install_omz
+        if [ "$install_omz" == "y" ]; then
+            if [ -d ~/.oh-my-zsh ]; then
+                echo "The \$ZSH folder already exists ($HOME/.oh-my-zsh)."
+                echo "You'll need to remove it if you want to reinstall."
+            else
+                install_oh_my_zsh
+                set_oh_my_zsh_theme_and_plugins
+                echo 'print_success_message' >> ~/.zshrc
+            fi
+        else
+            print_success_message
+        fi
+        
+        # Clone the repository
+        if [ -d ~/.fpd-shell ]; then
+            echo "~/.fpd-shell already exists. Please remove it or choose another directory."
+        else
+            echo "Cloning the FPD Shell repository..."
+            git clone https://github.com/FlowPress/fpd-shell.git ~/.fpd-shell
+            
+            # Source the main shell script in .bashrc or .zshrc
+            if [ -f ~/.zshrc ]; then
+                echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.zshrc
+                source ~/.zshrc
+                elif [ -f ~/.bashrc ]; then
+                echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.bashrc
+                source ~/.bashrc
+            else
+                # Default to creating .bashrc if neither exists
+                echo 'source ~/.fpd-shell/fpd-shell.sh' >> ~/.bashrc
+                source ~/.bashrc
+            fi
+        fi
     ;;
-  u|uninstall)
-    uninstall_fpd_shell
+    u|uninstall)
+        uninstall_fpd_shell
     ;;
-  *)
-    echo "Invalid option. Please choose 'i' for install or 'u' for uninstall."
+    *)
+        echo "Invalid option. Please choose 'i' for install or 'u' for uninstall."
     ;;
 esac
--- util/install_oh_my_zsh.sh
+++ util/install_oh_my_zsh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 install_oh_my_zsh() {
-  echo "Installing Oh My Zsh..."
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
