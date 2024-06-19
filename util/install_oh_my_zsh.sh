90942281054126fb80b9cc55e8b10a971720d8f7
Bart Dabek <bart@flowpress.com>
2024-06-19 11:20:39 -0400

    Installation work... wip

--- util/install_oh_my_zsh.sh
+++ util/install_oh_my_zsh.sh
@@ -2,11 +2,11 @@
 
 # Source the .fpd-shellrc file to get logging functions
 ls ~/
-source ~/.fpd-shellrc
+source ~/.fpd-shell/.fpd-shellrc
 
 install_oh_my_zsh() {
-    log_heading "Installing Oh My Zsh..."
-    RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	log_heading "Installing Oh My Zsh..."
+	RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 }
