83acc23318f8c9187a449f6af4e41eff885cd010
Bart Dabek <bart@flowpress.com>
2024-06-19 12:08:38 -0400

    Installation work... wip

--- util/uninstall_fpd_shell.sh
+++ util/uninstall_fpd_shell.sh
@@ -6,12 +6,12 @@ if [[ -f ~/.fpd-shell/.fpd-shellrc ]]; then
 fi
 
 uninstall_fpd_shell() {
-	log_heading "Uninstalling FPD Shell..."
+	echo "Uninstalling FPD Shell..."
 
 	# Remove FPD Shell directory
 	if [[ -d ~/.fpd-shell ]]; then
 		rm -rf ~/.fpd-shell
-		log_success "FPD Shell directory removed."
+		echo "FPD Shell directory removed."
 	fi
 
 	# Remove FPD Shell sourcing from .zshrc or .bashrc
@@ -22,11 +22,11 @@ uninstall_fpd_shell() {
 		sed -i '' '/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/d' ~/.zshrc
 		sed -i '' '/ZSH=$HOME\/.oh-my-zsh/d' ~/.zshrc
 		sed -i '' '/source $ZSH\/oh-my-zsh.sh/d' ~/.zshrc
-		log_success "Removed FPD Shell sourcing from .zshrc."
+		echo "Removed FPD Shell sourcing from .zshrc."
 	elif [[ -f ~/.bashrc ]]; then
 		sed -i '' '/source ~\/.fpd-shell\/fpd-shell.sh/d' ~/.bashrc
 		sed -i '' '/source ~\/.fpd-shell\/.fpd-shellrc/d' ~/.bashrc
-		log_success "Removed FPD Shell sourcing from .bashrc."
+		echo "Removed FPD Shell sourcing from .bashrc."
 	fi
 }
 
@@ -34,6 +34,6 @@ uninstall_fpd_shell() {
 uninstall_oh_my_zsh() {
 	if [[ -d ~/.oh-my-zsh ]]; then
 		sh ~/.oh-my-zsh/tools/uninstall.sh --unattended
-		log_success "Oh My Zsh uninstalled."
+		echo "Oh My Zsh uninstalled."
 	fi
 }
